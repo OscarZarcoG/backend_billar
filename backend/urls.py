6f5f590b10b3776eb65deab274088c954ccae798
OscarZarcoG <danielzargon@gmail.com>
2025-10-21 17:42:26 -0600

    fix main endpoint

--- backend/urls.py
+++ backend/urls.py
@@ -3,9 +3,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('AUTH.urls'))
+    path('api/', include('AUTH.urls')),
+    path('', lambda request: HttpResponse("Backend del sistema Billar funcionando ✅")),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
